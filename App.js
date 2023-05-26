42e2ee249a1c21968d6350f075816498d83d02c6
Ahkrolina <ahkrolina@gmail.com>
2023-05-26 03:03:59 +0000

    Iniciei o comparador falta estilizar melhor

--- App.js
+++ App.js
@@ -1,20 +1,75 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import React, { useState } from 'react';
+import { View, Text, TextInput, Button, StyleSheet } from 'react-native';
 
-export default function App() {
+const App = () => {
+  const [gasPrice, setGasPrice] = useState('');
+  const [ethanolPrice, setEthanolPrice] = useState('');
+  const [result, setResult] = useState('');
+
+  const comparePrices = () => {
+    const gasValue = parseFloat(gasPrice);
+    const ethanolValue = parseFloat(ethanolPrice);
+    const ethanolPercentage = (ethanolValue / gasValue) * 100;
+
+    if (ethanolPercentage > 70) {
+      setResult('Vale a pena comprar etanol.');
+    } else {
+      setResult('Vale a pena comprar gasolina.');
+    }
+  };
+
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
+      <Text style={styles.label}>Preço da Gasolina:</Text>
+      <TextInput
+        style={styles.input}
+        keyboardType="numeric"
+        value={gasPrice}
+        onChangeText={setGasPrice}
+      />
+
+      <Text style={styles.label}>Preço do Etanol:</Text>
+      <TextInput
+        style={styles.input}
+        keyboardType="numeric"
+        value={ethanolPrice}
+        onChangeText={setEthanolPrice}
+      />
+
+      <Button title="Comparar Preços" onPress={comparePrices} />
+
+      <Text style={styles.result}>{result}</Text>
     </View>
   );
-}
+};
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
     justifyContent: 'center',
+    alignItems: 'center',
+    padding: 16,
+    backgroundColor: 'lightblue',
+  },
+  label: {
+    fontSize: 18,
+    marginBottom: 8,
+    color: 'purple',
+  },
+  input: {
+    width: '100%',
+    height: 40,
+    borderWidth: 1,
+    borderColor: '#ccc',
+    marginBottom: 16,
+    paddingHorizontal: 8,
+    backgroundColor: 'lavender',
+  },
+  result: {
+    fontSize: 18,
+    marginTop: 16,
+    color: 'darkblue',
   },
 });
+
+export default App;
